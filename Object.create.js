2fdf40c1758c583f5b7a64e21ddc8c897cd50917
wandras <landzup@gmail.com>
2021-08-30 15:20:12 +0200

    Update Object.create.js
    Error message typo correction proposal.

--- Object.create.js
+++ Object.create.js
@@ -5,7 +5,7 @@
 if (typeof Object.create !== 'function') {
   Object.create = function (o) {
     if (arguments.length !== 1) {
-      throw new Error('Object.crete polyfill only accept one parameter');
+      throw new Error('Object.create polyfill only accepts one parameter');
     }
 
     function F() {}
